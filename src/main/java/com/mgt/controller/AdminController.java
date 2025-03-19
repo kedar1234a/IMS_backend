49bf42915e004668a1a293b911f8932b10d10858
Kedar1154 <kedarbhosale370@gmail.com>
2025-03-19 12:39:35 +0530

    Changes in Controller (/api and CrossOrigin)

--- src/main/java/com/mgt/controller/AdminController.java
+++ src/main/java/com/mgt/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.mgt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,8 @@ import com.mgt.model.Admin;
 import com.mgt.service.AdminService;
 
 @RestController
-@RequestMapping("/admin/user")
+@RequestMapping("/api/admin")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AdminController {
 
 	@Autowired
